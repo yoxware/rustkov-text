2049deca89eec9693db376c1cefee7e069353896
yoxware <wurze@protonmail.ch>
2021-04-21 23:18:25 -0400

    add reading corpus into dict functionality

--- src/corpus.rs
+++ src/corpus.rs
@@ -1,1 +1,66 @@
+use std::io::{BufReader, Read};
+use std::path::Path;
+use std::{collections::HashMap, io::BufRead};
 
+// enables specification of how the corpus will be read
+#[derive(Debug)]
+pub struct CorpusConfig {
+    n_gram_size: u32,
+    filters: Vec<String>,
+}
+
+pub fn open_corpus(file_paths: Vec<&str>) -> Result<Vec<std::fs::File>, std::io::Error> {
+    let mut corpus_vec: Vec<std::fs::File> = vec![];
+    for file in file_paths {
+        let path = Path::new(file);
+        let corpus = std::fs::File::open(path);
+        if corpus.is_err() {
+            let err_string: String = format!(
+                "Unable to open file {}; are you sure the path is correct?",
+                file
+            );
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                err_string.as_str(),
+            ));
+        }
+        corpus_vec.push(corpus.unwrap())
+    }
+    Ok(corpus_vec)
+}
+
+pub fn read_corpus(
+    corpus_vec: Vec<std::fs::File>,
+    corpus_conf: CorpusConfig,
+    gen_dictionary: &mut HashMap<String, Vec<String>>,
+) {
+    for corpus in corpus_vec {
+        let mut reader = BufReader::new(corpus);
+        let mut line = String::new();
+        reader.read_line(&mut line);
+        let mut split = line.split(" ");
+        let vec: Vec<&str> = split.collect();
+        for i in 0..vec.len() {
+            let mut n_gram = String::new();
+            for j in i..corpus_conf.n_gram_size as usize {
+                if (i + corpus_conf.n_gram_size as usize) > vec.len() - 1 {
+                    break;
+                } else {
+                    if j < i + corpus_conf.n_gram_size as usize - 1 {
+                        n_gram = n_gram + vec[i] + " ";
+                    } else {
+                        n_gram = n_gram + vec[i];
+                    }
+                }
+            }
+            let suffix = vec[i + corpus_conf.n_gram_size as usize];
+            if gen_dictionary.contains_key(&n_gram) {
+                let mut old_value = gen_dictionary.get(&n_gram).unwrap().clone();
+                old_value.push(suffix.to_owned());
+                gen_dictionary.insert(n_gram, old_value);
+            } else {
+                gen_dictionary.insert(n_gram, vec![suffix.to_owned()]);
+            }
+        }
+    }
+}
